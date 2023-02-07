0e461f568deab9127b6be4f4c791bca7b85adc07
Tahmid <tahmid.tf1@gmail.com>
2023-02-07 18:24:03 +0600

    start from 173

--- Vue-3-router-docs/vue router/src/components/teams/TeamMembers.vue
+++ Vue-3-router-docs/vue router/src/components/teams/TeamMembers.vue
@@ -22,11 +22,24 @@ export default {
     UserItem,
   },
   data() {
-    return {};
+    return {
+      teamName: '',
+      members: [],
+    };
   },
   created() {
     const teamId = this.$route.params.id;
-    console.log(this.teams.find((el) => (el.id = teamId)));
+    const selectedTeam = this.teams.find((el) => (el.id = teamId));
+    const members = selectedTeam.members;
+    const selectedMembers = [];
+
+    for (const member of members) {
+      const selectedUser = this.users.find((user) => user.id == member);
+      selectedMembers.push(selectedUser);
+    }
+
+    this.members = selectedMembers;
+    this.teamName = selectedTeam.name;
   },
 };
 </script>
